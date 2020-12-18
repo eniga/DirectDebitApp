ef7904557d76584e8f037460f0c45f97b9f97c09
Eniga Ahiante <enigaahiante@aol.com>
2020-12-18 01:29:20 +0100

    add all implemented services to start up class for DI

---
+++ DirectDebitApi/Services/Payment/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DirectDebitApi.Entities;
+using DirectDebitApi.Repositories;
+using MicroOrm.Dapper.Repositories;
+
+namespace DirectDebitApi.Services.Payment
+{
+    public class PaymentService : GenericRepository<Payments>, IPaymentService
+    {
+        public PaymentService(DapperRepository<Payments> repository): base(repository)
+        {
+        }
+    }
+}
