7af180c0e490e008e2a1c24c453349a4d05029bd
Eniga Ahiante <enigaahiante@aol.com>
2020-12-18 01:10:44 +0100

    implement contact, customer and disbursement service

---
+++ DirectDebitApi/Services/Customer/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DirectDebitApi.Entities;
+using DirectDebitApi.Repositories;
+
+namespace DirectDebitApi.Services.Customer
+{
+    public interface ICustomerService : IGenericRepository<Customers>
+    {
+    }
+}
