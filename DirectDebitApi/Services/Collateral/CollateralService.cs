35044ba5e1449319325b028a52ae20107dd94e06
Eniga Ahiante <enigaahiante@aol.com>
2020-12-18 01:03:14 +0100

    Implement collateral service

---
+++ DirectDebitApi/Services/Collateral/CollateralService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DirectDebitApi.Entities;
+using DirectDebitApi.Repositories;
+using MicroOrm.Dapper.Repositories;
+
+namespace DirectDebitApi.Services.Collateral
+{
+    public class CollateralService : GenericRepository<Collaterals>, ICollateralService
+    {
+        public CollateralService(DapperRepository<Collaterals> repository): base(repository)
+        {
+        }
+    }
+}
